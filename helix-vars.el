e72b97e85eff36f61513733a3fcd516faafc74aa
Yuriy Artemyev <anuvyklack@gmail.com>
2025-06-04 21:23:06 +0300

    refactor(undo): Group undo functionality together

--- helix-vars.el
+++ helix-vars.el
@@ -695,5 +695,21 @@ will be invoked if `helix--extend-selection' is t.")
   "The list of temporarily disabled minor-modes while there are
 multiple cursors.")
 
+(defvar helix--executing-command-for-fake-cursor nil
+  "Non-nil if `this-command' is currently executing for fake cursor.")
+
+(defvar helix--remove-post-command-hook nil
+  "If non-nil the `helix--post-command-hook' function will be removed from
+`post-command-hook' after execution.")
+
+(defvar helix--in-single-undo-step nil
+  "Non-nil while we are in the single undo step.")
+
+(defvar helix--undo-list-pointer nil
+  "Stores the start of the current undo step in `buffer-undo-list'.")
+
+(defvar helix--point-state-during-undo nil
+  "Hold state of the real cursor while a fake cursor active for undo.")
+
 (provide 'helix-vars)
 ;;; helix-vars.el ends here
