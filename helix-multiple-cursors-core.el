edd87eb7c9dd7167067da6751c7cd180ca0df640
Yuriy Artemyev <anuvyklack@gmail.com>
2025-06-05 15:08:41 +0300

    chore: Indentation

--- helix-multiple-cursors-core.el
+++ helix-multiple-cursors-core.el
@@ -549,6 +549,7 @@ activate this fake cursor and place it place into POSITION."
 
 (defmacro helix-save-window-scroll (&rest body)
   "Save the window scroll position, evaluate BODY, restore it."
+  (declare (indent 0) (debug t))
   (let ((win-start (make-symbol "win-start"))
         (win-hscroll (make-symbol "win-hscroll")))
     `(let ((,win-start (set-marker (make-marker) (window-start)))
@@ -583,10 +584,10 @@ evaluate BODY, update fake CURSOR."
   (declare (indent 0) (debug t))
   `(progn
      (helix-save-window-scroll
-      (helix-save-excursion
-       (dolist (cursor (helix-all-fake-cursors))
-         (helix-with-fake-cursor cursor
-           ,@body))))
+       (helix-save-excursion
+        (dolist (cursor (helix-all-fake-cursors))
+          (helix-with-fake-cursor cursor
+            ,@body))))
      ;; Finaly execute for real cursor
      ,@body))
 
@@ -626,10 +627,10 @@ the original cursor, to inform about the lack of support."
                      (memq command helix-commands-to-run-for-all-cursors)
                      (helix--prompt-for-unknown-command command))))
            (helix-save-window-scroll
-            (helix-save-excursion
-             (dolist (cursor (helix-all-fake-cursors))
-               (helix-with-fake-cursor cursor
-                 (call-interactively command)))))))))
+             (helix-save-excursion
+              (dolist (cursor (helix-all-fake-cursors))
+                (helix-with-fake-cursor cursor
+                  (call-interactively command)))))))))
 
 (defmacro helix-with-real-cursor-as-fake (&rest body)
   "Temporarily create a fake-cursor for real one with ID 0.
