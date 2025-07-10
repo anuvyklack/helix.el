73a6710c3b1e6aa14c59f26464b425e89297944e
Yuriy Artemyev <anuvyklack@gmail.com>
2025-07-10 11:46:31 +0300

    fix(core): Temporary solution for intercepting Edebug

--- helix-core.el
+++ helix-core.el
@@ -39,7 +39,13 @@
 
 (defun helix--pre-commad-hook ()
   (unless helix--executing-command-for-fake-cursor
-    (setq helix-this-command this-command)
+    ;; FIXME: Need to intercept into Edebug: when in Edebug `this-command'
+    ;; in `pre-command-hook' is the Edebugs command you invoced, and then
+    ;; Edebug set it to correct value inside somewhere inside this Edebug
+    ;; command. So we need to find where does it happen and add advice to
+    ;; set `helix-this-command' to this value.
+    (unless  edebug-mode ;; this is bad termporary solution
+      (setq helix-this-command this-command))
     (when (and (symbolp this-command)
                (get this-command 'helix-deactivate-mark))
       (deactivate-mark))
