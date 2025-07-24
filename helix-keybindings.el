554f96f678fe1ad919869a85f4665fe37629d6a7
Yuriy Artemyev <anuvyklack@gmail.com>
2025-07-24 13:53:08 +0300

    Add [s and ]s commands to move over sentences

--- helix-keybindings.el
+++ helix-keybindings.el
@@ -77,6 +77,8 @@
   "{"   #'helix-backward-paragraph
   "] f" #'helix-forward-function
   "[ f" #'helix-backward-function
+  "] s" #'helix-forward-sentence
+  "[ s" #'helix-backward-sentence
 
   ;; Easymotion / Avy
   "g w" #'helix-avy-word-forward
