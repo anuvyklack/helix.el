0b6415a869f1564ea9828afb8e251c7a322d65dd
Yuriy Artemyev <anuvyklack@gmail.com>
2025-08-04 00:39:38 +0300

    Rename `helix-executing-command-for-fake-cursor` variable

--- helix-vars.el
+++ helix-vars.el
@@ -447,7 +447,10 @@ It is active while there are multiple cursors.")
 
 (helix-defvar-local helix-this-command nil
   "Like `this-command' but for fake cursors.
-The command that that will be executed by each fake cursor.")
+The command that that will be executed for each fake cursor.")
+
+(helix-defvar-local helix-executing-command-for-fake-cursor nil
+  "Non-nil if `this-command' is currently executing for fake cursor.")
 
 (helix-defvar-local helix--temporarily-disabled-minor-modes nil
   "The list of temporarily disabled minor-modes while there are
