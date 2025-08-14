1a54a6c1035b4c1d89d4031d93e858108d02627d
Yuriy Artemyev <anuvyklack@gmail.com>
2025-08-15 00:59:41 +0300

    Add copyright

--- helix-common.el
+++ helix-common.el
@@ -1,5 +1,7 @@
 ;;; helix-common.el --- Common functions -*- lexical-binding: t; -*-
 ;;
+;; Copyright (C) 2025 Yuriy Artemyev
+;;
 ;; Author: Yuriy Artemyev <anuvyklack@gmail.com>
 ;; Maintainer: Yuriy Artemyev <anuvyklack@gmail.com>
 ;; Version: 0.0.1
