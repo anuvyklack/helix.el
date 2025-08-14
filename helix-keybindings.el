bfd1ce10227fcd9340158599640041b21e5fdfd7
Yuriy Artemyev <anuvyklack@gmail.com>
2025-08-14 13:23:10 +0300

    feat: C-w in insert state delete backward word

--- helix-keybindings.el
+++ helix-keybindings.el
@@ -316,7 +316,8 @@
 ;;; Insert state
 
 (helix-keymap-set nil 'insert
-  "<escape>" #'helix-normal-state)
+  "<escape>" #'helix-normal-state
+  "C-w" #'helix-delete-backward-word)
 
 (provide 'helix-keybindings)
 ;;; helix-keybindings.el ends here
