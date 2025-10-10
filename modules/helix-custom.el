ad5aa830f66f8a2b6c9f4dc250aeb555557400df
Yuriy Artemyev <anuvyklack@gmail.com>
2025-10-10 04:59:11 +0300

    feat: customize module

---
+++ modules/helix-custom.el
@@ -0,0 +1,29 @@
+;;; helix-custom.el -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Yuriy Artemyev
+;;
+;; Author: Yuriy Artemyev <anuvyklack@gmail.com>
+;; Maintainer: Yuriy Artemyev <anuvyklack@gmail.com>
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "29.1"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;; Keybindings for Customize interface.
+;;
+;;; Code:
+(require 'helix-core)
+(require 'cus-edit)
+
+(helix-set-initial-state 'Custom-mode 'normal)
+
+(helix-keymap-set custom-mode-map :state 'normal
+  "z j" #'widget-forward
+  "z k" #'widget-backward
+  "z u" #'Custom-goto-parent
+  "q"   #'Custom-buffer-done)
+
+(provide 'helix-custom)
+;;; helix-custom.el ends here
